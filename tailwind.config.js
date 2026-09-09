1f84468bb5e385284d1eeb728852189d4375a349
erinshearer48-cpu <erinshearer48@gmail.com>
2026-09-09 05:05:53 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        teal: { DEFAULT: '#5FBFB3', dark: '#3E8F86' },
+        navy: '#0F1B2D',
+        sand: '#F7F7F5',
+        gold: '#F6C90E'
+      },
+      fontFamily: {
+        sans: ['Arial', 'Helvetica', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
